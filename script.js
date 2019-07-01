923a538d73ddaf0a1e55d57d30bd44e0fec349a3
Eugene <zhenye.parashak@gmail.com>
2019-07-01 15:38:28 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';   
+
+
+
+var LeftBorderWidth =1;
+
+let second = 3;
+console.log(second);
+const pi = 3.14;
+
+let person={
+    name:"John",
+    age:25,
+    isMarried:false
+
+};
+alert("Привет");
+console.log(person.isMarried);
